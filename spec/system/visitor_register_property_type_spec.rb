92ea06b715231ec6c18f72b0153dac5532f6e5ff
angelasoler <manzanaembicicleta@gmail.com>
2021-10-02 14:23:19 -0300

    cadastro do tipo de imóvel spec-type error

--- spec/system/visitor_register_property_type_spec.rb
+++ spec/system/visitor_register_property_type_spec.rb
@@ -28,17 +28,17 @@ describe 'Visitor register property type' do
     expect(page).to have_content('Nome do tipo do seu imóvel')
   end
 
-  # it 'and ' do
-  #   property_owner = PropertyOwner.create!(email: 'jane@doe.com.br', password: '123456789')
-  #   PropertyType.create!(name: 'Casa')
+  it 'and register and exisiting type' do
+    property_owner = PropertyOwner.create!(email: 'jane@doe.com.br', password: '123456789')
+    PropertyType.create!(name: 'Casa')
       
-  #   visit root_path
-  #   click_on 'Cadastrar Imóvel'
-  #   click_on 'Cadastrar Tipo De Imóvel'
-  #   fill_in 'Digite o tipo do seu imóvel', with: 'Casa'
-  #   click_on 'Cadastrar'
+    visit root_path
+    click_on 'Cadastrar Imóvel'
+    click_on 'Cadastrar Tipo De Imóvel'
+    fill_in 'Digite o tipo do seu imóvel', with: 'Casa'
+    click_on 'Cadastrar'
 
-  #   expect(page).to have_content('Esse tipo de imóvel já exite')
-  #   expect(page).to have_content('Cadastro de Imóvel')
-  # end
+    expect(page).to have_content('Esse tipo de imóvel já exite')
+    expect(page).to have_content('Cadastro de Imóvel')
+  end
 end
