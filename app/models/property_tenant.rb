94e10b153ba2627efd1894593a802bf094e72425
angelasoler <manzanaembicicleta@gmail.com>
2021-10-06 11:08:26 -0300

    create property tenant devise

---
+++ app/models/property_tenant.rb
@@ -0,0 +1,6 @@
+class PropertyTenant < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
