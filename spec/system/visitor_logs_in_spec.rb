6623e3e7034372813095c5ac144a14b6075b1e8e
angelasoler <manzanaembicicleta@gmail.com>
2021-10-08 12:55:14 -0300

    WIP tradução de viwes devise e usuario property_tenant

--- spec/system/visitor_logs_in_spec.rb
+++ spec/system/visitor_logs_in_spec.rb
@@ -10,7 +10,7 @@ describe 'Visitor log in' do
       fill_in 'Email', with: jane.email
       fill_in 'Senha', with: jane.password
       within 'form' do
-      click_on 'Entrar'
+        click_on 'Entrar'
       end
 
       expect(page).to have_content('Login efetuado com sucesso!')
@@ -38,6 +38,7 @@ describe 'Visitor log in' do
       
       visit root_path
       click_on 'Crie uma conta'
+      check 'Sou Proprietario'
       fill_in 'Email', with: 'fakemail@fake.com'
       fill_in  'Senha', with: '123456'
       fill_in 'Confirmação de senha', with: '123456'
@@ -50,7 +51,55 @@ describe 'Visitor log in' do
       # email, senha e confirmação de senha
     end
     
-    context 'as user' do
+    context 'as user/property tenant' do
+      it 'successfully' do
+        luis = PropertyOwner.create!(email: 'luis@doe.com.br', password: '123456789')
+  
+        visit root_path
+        click_on 'Entrar'
+        fill_in 'Email', with: luis.email
+        fill_in 'Senha', with: luis.password
+        within 'form' do
+        click_on 'Entrar'
+        end
+  
+        expect(page).to have_content('Login efetuado com sucesso!')
+        expect(page).to have_content(luis.email)
+        expect(page).to have_link('Logout')
+        expect(page).not_to have_link('Entrar')
+        expect(page).to have_link('Cadastrar Imóvel')
+      end
+  
+      it 'and logs out' do
+        luis = PropertyOwner.create!(email: 'luis@doe.com.br', password: '123456789')
+  
+        login_as jane, scope: :property_owner
+        visit root_path
+        click_on 'Logout'
+        
+        expect(page).to have_content('Saiu com sucesso')
+        expect(page).to_not have_content(luis.email)
+        expect(page).to_not have_link('Logout')
+        expect(page).to have_content('Entrar')
+        expect(page).to_not have_link('Cadastrar Imóvel')
+      end
+  
+      it 'and create an account' do
+        
+        visit root_path
+        click_on 'Crie uma conta'
+        check 'Sou Locatario'
+        fill_in 'Email', with: 'fakemail@fake.com'
+        fill_in  'Senha', with: '123456'
+        fill_in 'Confirmação de senha', with: '123456'
+        click_on 'Criar Conta'
+  
+        expect(page).to have_content('Conta cadastrada com sucesso!!')
+        expect(page).to have_link('Cadastrar Imóvel')
+        expect(page).to have_content('fakemail@fake.com')
+        expect(page).not_to have_link('Entrar')
+        # email, senha e confirmação de senha
+      end
     end
   end
 end
